82f450ccdb9462575f119b09980edc23e83089c2
Aleksandr-Fi <90216724+Aleksandr-Fi@users.noreply.github.com>
2022-01-26 23:03:26 +0300

    I remembered fletch and promis

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 console.log('hello');
+
+const getResourse = async (url) => {
+    const res = await fetch(url)
+    const body = await res.json()
+    return body
+}
+
+getResourse('https://swapi.dev/api/peple/1/') // true: 'https://swapi.dev/api/people/1/'
+    .then((body) => {
+        console.log(body);
+    })
+    .then(() => {
+        console.log('sucsess');
+    },
+    () => {
+        console.log('fail');
+    }
+    )
