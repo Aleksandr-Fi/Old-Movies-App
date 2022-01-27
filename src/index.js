784736adf3f78b2559c5265316969a6c9ad0604d
Aleksandr-Fi <90216724+Aleksandr-Fi@users.noreply.github.com>
2022-01-27 16:33:49 +0300

    c3

--- src/index.js
+++ src/index.js
@@ -1,46 +1,8 @@
+import MovieService from './components/movie-service'
+
 console.log('hello');
 
-class SwapiService {
-
-    _apiBase = 'https://swapi.dev/api'
-
-    async getResourse(url) {
-        const res = await fetch(`${this._apiBase}${url}`)
-        if (!res.ok) {
-            throw new Error(`Could not fetch ${this._apiBase + url}, received ${res.status}`)
-        }
-        return await res.json()
-    }
-
-    async getAllPeople() {
-        const res = await this.getResourse('/people/')
-        return res.results
-    }
-
-    getPerson(id) {
-        return this.getResourse(`/people/${id}/`)
-    }
-
-    async getAllPlanets() {
-        const res = await this.getResourse('/planets/')
-        return res.results
-    }
-
-    getPlanet(id) {
-        return this.getResourse(`/planets/${id}/`)
-    }
-
-    async getAllStarships() {
-        const res = await this.getResourse('/starships/')
-        return res.results
-    }
-
-    getStarship(id) {
-        return this.getResourse(`/starships/${id}/`)
-    }
-}
-
-const swapi = new SwapiService()
+const movie = new MovieService()
 
 // swapi.getAllPeople().then((people) => {
 //     people.forEach(p => {
@@ -48,6 +10,6 @@ const swapi = new SwapiService()
 //     });
 // })
 
-swapi.getPerson(3).then((person) => {
+movie.getPerson(3).then((person) => {
     console.log(person.name);
 })
