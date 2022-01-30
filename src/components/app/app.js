bc9dc6f57edd103bba2b86b652ba92dd8fab8a69
Aleksandr-Fi <90216724+Aleksandr-Fi@users.noreply.github.com>
2022-01-30 19:38:36 +0300

    add image

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -5,8 +5,7 @@ import Footer from '../footer'
 import './app.css'
 
 export default class App extends Component {
-
-    maxId = 1 
+    maxId = 1
 
     state = {
         cardData: [
@@ -15,17 +14,21 @@ export default class App extends Component {
             this.createCardFilm(),
             this.createCardFilm(),
             this.createCardFilm(),
-            this.createCardFilm()
-        ]
+            this.createCardFilm(),
+        ],
     }
 
     createCardFilm() {
         return {
             id: this.maxId,
-            title: `title ${this.maxId}`,
-            description: `description ${this.maxId++}`
+            title: `The way back ${this.maxId++}`,
+            premier: `March 5, 2020`,
+            genre: ['Action', 'Drama'],
+            description: `A former basketball all-star, who has lost his wife and family foundation in a struggle with 
+                addiction attempts to regain his soul  and salvation by becoming the coach 
+                of a disparate ethnically mixed high ...`,
         }
-    }    
+    }
 
     render() {
         const { cardData } = this.state
